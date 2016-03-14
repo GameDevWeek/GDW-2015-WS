9453b273a459705d7e920514a8642e07e28cb56a
GIumbatsch <davidsiepen@outlook.com>
2016-03-14 14:02:01 +0100

    PhysicsLoader fuer PhysicTeam

---
+++ gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/game/utils/EntityLoader.java
@@ -0,0 +1,18 @@
+package de.hochschuletrier.gdw.ws1516.game.utils;
+
+import com.badlogic.ashley.core.PooledEngine;
+
+import de.hochschuletrier.gdw.commons.tiled.TiledMap;
+import de.hochschuletrier.gdw.ws1516.game.Game;
+
+public class EntityLoader implements MapLoader{
+
+	@Override
+	public void parseMap(TiledMap map, Game game, PooledEngine engine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
