cbe5d8950357cef667de852585fc1145dc9c6ed3
Baruschi <Saradabaschi@hotmail.de>
2016-03-14 14:33:43 +0100

    Test

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ws1516/menu/MenuPageRoot.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ws1516/menu/MenuPageRoot.java
@@ -1,6 +1,11 @@
 package de.hochschuletrier.gdw.ws1516.menu;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
 import de.hochschuletrier.gdw.commons.gdx.menu.MenuManager;
 import de.hochschuletrier.gdw.commons.gdx.state.transition.SplitHorizontalTransition;
 import de.hochschuletrier.gdw.ws1516.game.Game;
@@ -19,19 +24,25 @@ public class MenuPageRoot extends MenuPage {
         super(skin, "menu_bg");
 
 //        addActor(new DecoImage(assetManager.getTexture("menu_bg_root_bottom")));
-        int x = 100;
+        int x = 600;
         int i = 0;
         int y = 370;
         int yStep = 55;
         if (type == Type.MAINMENU) {
-            addLeftAlignedButton(x, y - yStep * (i++), 400, 50, "Spiel NICHHT Starten", this::startGame);
-            
+            addLeftAlignedButton(x, y - yStep * (i++), 400, 50, "Spiel Starten", this::startGame);
         } else {
             addLeftAlignedButton(x, y - yStep * (i++), 400, 50, "Fortsetzen", () -> menuManager.popPage());
             addLeftAlignedButton(x, y - yStep * (i++), 400, 50, "Spiel verlassen", this::stopGame);
         }
         addPageEntry(menuManager, x, y - yStep * (i++), "Credits", new MenuPageCredits(skin, menuManager));
         addCenteredButton(menuManager.getWidth() - 80, 54, 100, 40, "Exit", () -> System.exit(-1));
+        addCenteredButton(menuManager.getWidth()-200,200,200,200, "Testest", () -> System.exit(-1));
+        TextButton button = new TextButton("NeuerTestButton",skin,"default");
+        button.setBounds(200, 200, 800, 100);
+        button.getLabel().setAlignment(Align.center);
+        addActor(button);
+        
+        
     }
 
     private void startGame() {
