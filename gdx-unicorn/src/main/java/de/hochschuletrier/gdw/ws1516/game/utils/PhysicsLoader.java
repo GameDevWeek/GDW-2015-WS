cc8b699d53c235be13c2212ad5a0fa3d3acc9776
Eileen Neumann <neumann.eileen.1992@gmail.com>
2016-03-14 14:47:23 +0100

    opened physic branch

--- gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/game/utils/PhysicsLoader.java
+++ gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/game/utils/PhysicsLoader.java
@@ -2,20 +2,37 @@ package de.hochschuletrier.gdw.ws1516.game.utils;
 
 import com.badlogic.ashley.core.PooledEngine;
 
+import de.hochschuletrier.gdw.commons.tiled.Layer;
+import de.hochschuletrier.gdw.commons.tiled.TileInfo;
 import de.hochschuletrier.gdw.commons.tiled.TiledMap;
+import de.hochschuletrier.gdw.commons.tiled.utils.RectangleGenerator;
+import de.hochschuletrier.gdw.commons.utils.Rectangle;
 import de.hochschuletrier.gdw.ws1516.game.Game;
 
 public class PhysicsLoader implements MapLoader {
 
 	
-	public PhysicsLoader(){
-		
-	}
+    public PhysicsLoader(){
+        
+    }
 
-	@Override
-	public void parseMap(TiledMap map, Game game, PooledEngine engine) {
-		
-	}
+    @Override
+    public void parseMap(final TiledMap map, final Game game, final PooledEngine engine) {
+        
+        // Generate static world
+        //TODO : test
+        int tileWidth = map.getTileWidth();
+        int tileHeight = map.getTileHeight();
+        RectangleGenerator generator = new RectangleGenerator();
+        generator.generate(map,
+                (final Layer layer, final TileInfo info) -> info.getBooleanProperty("blocked", false),
+                (final Rectangle rect) -> addSolidRectangle(rect, tileWidth, tileHeight));
+        
+    }
+    
+    private void addSolidRectangle(final Rectangle rect, final int tileWidth, final int tileHeight) {
+        
+    }
 	
 	
 }
