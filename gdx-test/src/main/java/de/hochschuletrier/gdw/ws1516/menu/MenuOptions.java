89c0ef0fa68999176b62789f5bc9d9c841901af0
Baruschi <Saradabaschi@hotmail.de>
2016-03-14 16:06:10 +0100

    no message

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ws1516/menu/MenuOptions.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ws1516/menu/MenuOptions.java
@@ -2,9 +2,11 @@ package de.hochschuletrier.gdw.ws1516.menu;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
+import de.hochschuletrier.gdw.commons.gdx.menu.MenuManager;
+
 public class MenuOptions extends MenuPage {
 
-    public MenuOptions(Skin skin) {
+    public MenuOptions(Skin skin, MenuManager menuManager) {
         super(skin, "menu_bg");
        
         int x = 200;
@@ -12,9 +14,17 @@ public class MenuOptions extends MenuPage {
         int y = 370;
         
         int yStep = 55;
-       
+        addLeftAlignedButton(x,y-yStep * (i++),400,50,"Video",this::enterVideoOptions);
+        addLeftAlignedButton(x,y-yStep * (i++),400,50,"Audio",this::enterSoundOptions);
+        addLeftAlignedButton(50,50 ,200,50,"Audio",this::enterSoundOptions);
     }
     
+    
+    private void enterSoundOptions(){
+    }
+    
+    private void enterVideoOptions(){
+    }
     
 
     public static void main(String[] args) {
