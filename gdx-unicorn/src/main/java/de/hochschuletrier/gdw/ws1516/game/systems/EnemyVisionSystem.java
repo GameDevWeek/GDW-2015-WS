df53a1697d726e257b495ed1dbc2f414521d92a1
Qouve <philipp.martschei@gmx.net>
2016-03-15 16:45:51 +0100

    created EnemyVisionSystem

---
+++ gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/game/systems/EnemyVisionSystem.java
@@ -0,0 +1,54 @@
+package de.hochschuletrier.gdw.ws1516.game.systems;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.EntityListener;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+
+import de.hochschuletrier.gdw.commons.gdx.physix.components.PhysixBodyComponent;
+import de.hochschuletrier.gdw.ws1516.game.ComponentMappers;
+import de.hochschuletrier.gdw.ws1516.game.GameConstants;
+import de.hochschuletrier.gdw.ws1516.game.components.PlayerComponent;
+import de.hochschuletrier.gdw.ws1516.game.components.PositionComponent;
+
+public class EnemyVisionSystem extends IteratingSystem implements EntityListener{
+
+    private Entity unicorn = null;
+    
+    public EnemyVisionSystem() {
+        super(Family.all(EnemyBehaviourComponent.class,PositionComponent.class).get());
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        PositionComponent enemyPosition=ComponentMappers.position.get(entity);
+        EnemyBehaviourComponent enemyBehaviour=ComponentMappers.enemyBehaviour.get(entity);
+        PositionComponent unicornPosition=ComponentMappers.position.get(unicorn);
+        PhysixBodyComponent unicornBody=ComponentMappers.physixBody.get(unicorn);
+        float xDist=enemyPosition.x-unicornPosition.x;
+        float yDist=enemyPosition.y-unicornPosition.y;
+        float dist=(float)Math.sqrt((xDist*xDist)+(yDist*yDist));
+        
+        if (dist<=GameConstants.GLOBAL_VISION*GameConstants.UNICORN_SIZE){
+            enemyBehaviour.canSeeUnicorn=true;
+        }else{
+            enemyBehaviour.canSeeUnicorn=false;
+        }
+    }
+
+    @Override
+    public void entityAdded(Entity entity) {
+        if (entity.getComponent(PlayerComponent.class)!=null && entity.getComponent(PositionComponent.class)!=null){
+            unicorn=entity;
+        }
+    }
+
+    @Override
+    public void entityRemoved(Entity entity) {
+        if (entity==unicorn){
+            unicorn=null;
+        }
+    }
+
+}
