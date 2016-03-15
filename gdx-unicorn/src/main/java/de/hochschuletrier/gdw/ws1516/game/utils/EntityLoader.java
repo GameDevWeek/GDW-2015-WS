ebea030fe354cf1b30a3fca4a114c1bcbbad3aa6
GIumbatsch <davidsiepen@outlook.com>
2016-03-15 12:46:59 +0100

    addEntity func extracted

--- gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/game/utils/EntityLoader.java
+++ gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/game/utils/EntityLoader.java
@@ -17,7 +17,6 @@ import de.hochschuletrier.gdw.ws1516.game.ComponentMappers;
 import de.hochschuletrier.gdw.ws1516.game.Game;
 import de.hochschuletrier.gdw.ws1516.game.components.NameComponent;
 import de.hochschuletrier.gdw.ws1516.game.components.PathComponent;
-import de.hochschuletrier.gdw.ws1516.game.systems.NameSystem;
 
 public class EntityLoader implements MapLoader {
 
@@ -29,26 +28,10 @@ public class EntityLoader implements MapLoader {
         for (Layer layer : map.getLayers()) {
             if (layer.isObjectLayer()) {
                 for (LayerObject obj : layer.getObjects()) {
-                    String name = obj.getProperty("entity_type", null);
-
-                    if (name != null) {
-                        final String path = obj.getProperty("path", null);
-                        final float x = obj.getX() + obj.getWidth() * 0.5f;
-                        final float y = obj.getY() - obj.getHeight() * 0.5f;
-                        final float speed = obj.getFloatProperty("speed", 0.f);
-                        final boolean loop = obj.getBooleanProperty("loop", false);
-                        Entity e = EntityCreator.createEntity(name, path, x, y, speed, loop);
-                        NameComponent nc = engine.createComponent(NameComponent.class);
-                        nc.name = path;
-                        e.add(nc);
-                        if (name.equals("path")) {
-                            PathComponent pc = ComponentMappers.path.get(e);
-                            for (Point p : obj.getPoints()) {
-                                pc.points.add(new Vector2(p.x, p.y));
-                            }
-                            logger.info("{}", e.getComponent(PathComponent.class).points);
-                        }
-                        names.add(name);
+                    // only load tiles and rectangles
+                    LayerObject.Primitive primitive = obj.getPrimitive();
+                    if (primitive == LayerObject.Primitive.TILE || primitive == LayerObject.Primitive.POLYLINE) {
+                        addEntity(engine, names, obj);
                     }
                 }
             }
@@ -56,6 +39,30 @@ public class EntityLoader implements MapLoader {
 
         // Test Print
         logger.info("{}", names);
+    }
+
+    private void addEntity(PooledEngine engine, LinkedList<String> names, LayerObject obj) {
+        String name = obj.getProperty("entity_type", null);
+
+        if (name != null) {
+            final String path = obj.getProperty("path", null);
+            final float x = obj.getX() + obj.getWidth() * 0.5f;
+            final float y = obj.getY() - obj.getHeight() * 0.5f;
+            final float speed = obj.getFloatProperty("speed", 0.f);
+            final boolean loop = obj.getBooleanProperty("loop", false);
+            Entity e = EntityCreator.createEntity(name, path, x, y, speed, loop);
+            NameComponent nc = engine.createComponent(NameComponent.class);
+            nc.name = path;
+            e.add(nc);
+            if (name.equals("path")) {
+                PathComponent pc = ComponentMappers.path.get(e);
+                for (Point p : obj.getPoints()) {
+                    pc.points.add(new Vector2(p.x, p.y));
+                }
+                logger.info("{}", e.getComponent(PathComponent.class).points);
+            }
+            names.add(name);
+        }
     }
 
 }
