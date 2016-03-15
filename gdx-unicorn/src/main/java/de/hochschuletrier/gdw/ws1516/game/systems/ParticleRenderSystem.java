a60d8cb90de41cef8b4839d6436bcebc40ea7bcd
Michi <muffinmichi@gmx.de>
2016-03-15 13:25:21 +0100

    ParticleRendering initialisation finished

--- gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/game/systems/ParticleRenderSystem.java
+++ gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/game/systems/ParticleRenderSystem.java
@@ -2,19 +2,13 @@ package de.hochschuletrier.gdw.ws1516.game.systems;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import de.hochschuletrier.gdw.commons.gdx.ashley.SortedSubIteratingSystem;
-import de.hochschuletrier.gdw.commons.gdx.ashley.SortedSubIteratingSystem.SubSystem;
 import de.hochschuletrier.gdw.commons.gdx.utils.DrawUtil;
 import de.hochschuletrier.gdw.ws1516.game.ComponentMappers;
-import de.hochschuletrier.gdw.ws1516.game.components.AnimationComponent;
 import de.hochschuletrier.gdw.ws1516.game.components.ParticleTestComponent;
 import de.hochschuletrier.gdw.ws1516.game.components.PositionComponent;
-import de.hochschuletrier.gdw.ws1516.game.components.RenderLayerComponent;
 
 public class ParticleRenderSystem extends SortedSubIteratingSystem.SubSystem 
 {
@@ -28,7 +22,6 @@ public class ParticleRenderSystem extends SortedSubIteratingSystem.SubSystem
     public void processEntity(Entity entity, float deltaTime) 
     {        
 //        System.out.println("ParticleRenderSystem Process");
-        SpriteBatch batch = new SpriteBatch();
         
         ParticleTestComponent component = ComponentMappers.particleTest.get(entity);
         if(component != null)
@@ -41,14 +34,11 @@ public class ParticleRenderSystem extends SortedSubIteratingSystem.SubSystem
         }
         PositionComponent position = ComponentMappers.position.get(entity);
         
-        
         for(ParticleEmitter emitter : component.effect.getEmitters())
         {
-            batch.begin();
             emitter.start();
-            emitter.update(deltaTime);
             emitter.setPosition(position.x, position.y);
-            emitter.draw(batch, deltaTime);
+            emitter.draw(DrawUtil.batch, deltaTime);
         }
         component.effect.setPosition(position.x, position.y);
         component.effect.draw(DrawUtil.batch);
