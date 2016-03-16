e3fa672d649fc2433158c5ac3004d9c0696982b9
tobias gepp <object.name@live.de>
2016-03-16 12:55:31 +0100

    implemented Hnadling System to sandbox

---
+++ gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/game/systems/enemyStates/FollowPlayerEnemyState.java
@@ -0,0 +1,35 @@
+package de.hochschuletrier.gdw.ws1516.game.systems.enemyStates;
+
+import com.badlogic.ashley.core.Entity;
+
+import de.hochschuletrier.gdw.ws1516.events.EnemyActionEvent;
+import de.hochschuletrier.gdw.ws1516.game.ComponentMappers;
+import de.hochschuletrier.gdw.ws1516.game.components.EnemyBehaviourComponent;
+import de.hochschuletrier.gdw.ws1516.game.components.PositionComponent;
+import de.hochschuletrier.gdw.ws1516.game.systems.EnemyHandlingSystem.Action.Type;
+
+public class FollowPlayerEnemyState extends EnemyBaseState {
+
+    @Override
+    public EnemyBaseState _compute(Entity entity, Entity player, float deltaTime) {
+    
+        EnemyBehaviourComponent behaviour=ComponentMappers.enemyBehaviour.get(entity);
+        PositionComponent positionEnemy=ComponentMappers.position.get(entity);
+        PositionComponent positionPlayer=ComponentMappers.position.get(player);
+        if ( positionPlayer.x < positionEnemy.x ){
+            EnemyActionEvent.emit( entity,Type.MOVE ,-10.f);
+        }else
+        {
+            EnemyActionEvent.emit( entity,Type.MOVE ,10.f);   
+        }
+        
+        if ( behaviour.canSeeUnicorn )
+        {
+            return this;
+        }else
+        {
+            return new FollowPathEnemyState();
+        }
+    }
+
+}
