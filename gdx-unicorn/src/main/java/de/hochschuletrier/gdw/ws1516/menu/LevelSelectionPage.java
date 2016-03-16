5721651efa60e183a1fe8da2d74538a876563a0e
unknown <rftpool13@rft1312.rz.fh-trier.de>
2016-03-16 13:29:51 +0100

    first draft level selection page and replaced coins with hearts in HUD

--- gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/menu/LevelSelectionPage.java
+++ gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/menu/LevelSelectionPage.java
@@ -1,8 +1,13 @@
 package de.hochschuletrier.gdw.ws1516.menu;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 
 import de.hochschuletrier.gdw.commons.gdx.menu.MenuManager;
 import de.hochschuletrier.gdw.commons.gdx.state.transition.SplitHorizontalTransition;
@@ -25,11 +30,38 @@ public class LevelSelectionPage extends MenuPage {
         
         
         addLeftAlignedButton(xOffset, yOffset - yStep *( i++), 150, 50, "Start Game", this::startGame);
-        Texture texture = new Texture("data/dummies/coin.png");
+        
+        
+        
         Main.getInstance().screenCamera.bind();
-        DrawUtil.draw(texture, 20, 20, 50, 50);
-       // ImageButton image = new ImageButton(texture, 20, 20);
+        Texture level_preview_texture = new Texture("data/graphics/unicorn_s.png");
+        Texture buttonBack_texture = new Texture("data/graphics/blue_gum_s.png");
+        Texture buttonNext_texture = new Texture("data/graphics/blue_gum_s.png");
+        Texture[] level_previews = new Texture[5];
+        addImageButton(level_preview_texture, 450, 250, 50, 50, this::startGame);
+        addImageButton(buttonBack_texture, 430, 230, 50, 50, this::startGame);
+        addImageButton(buttonNext_texture, 450+level_preview_texture.getWidth(), 230, 50, 50, this::startGame);
         addLeftAlignedButton(xOffset, yOffset - yStep*(i++), 100, 50, "Menu", () -> menuManager.popPage());
+        
+    }
+    
+    private void addImageButton(Texture texture, float x, float y, float width, float height, Runnable runnable) {
+       ImageButton ib = new ImageButton(new SpriteDrawable(new Sprite(texture)));
+       
+       ib.addListener(new ClickListener() {
+           @Override
+           public void clicked(InputEvent event, float x, float y) {
+               runnable.run();
+           }
+       });
+       ib.setPosition(x, y);
+      // ib.setBounds(x, y, width, height);
+       
+       
+       super.addActor(ib);
+    }
+    
+    private void nextLevel() {
         
     }
     
