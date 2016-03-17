b84fdb386e7fbdadf6512985cd79c55b6ebd414b
unknown <rftpool13@rft1311.rz.fh-trier.de>
2016-03-17 14:17:44 +0100

    little update

--- gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/menu/MenuPage.java
+++ gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/menu/MenuPage.java
@@ -66,7 +66,7 @@ public class MenuPage extends Group {
         slider.setValue(50);
         
 
-        label.setPosition(x, y+7);
+        label.setPosition(x-5, y+7);
         slider.setBounds(x+75,y,size,35);
         Label value= new Label(""+(int)slider.getValue(),skin,"default");
         value.setPosition(x+size+85,y+7);   
