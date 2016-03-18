2239d67d3704e4bd84a3c191de6ff8a739ba134c
WeraGossner <W.Gossner@gmx.net>
2016-03-18 15:53:34 +0100

    Aktualisierte Tilesets

--- gdx-unicorn/src/main/resources/data/maps/tilesets/platform3x1.tsx
+++ gdx-unicorn/src/main/resources/data/maps/tilesets/platform3x1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="platform3x1" tilewidth="192" tileheight="32" tilecount="1" columns="1">
- <image source="tilesets/platform3x1.png" width="192" height="32"/>
+ <image source="platform3x1.png" width="192" height="32"/>
 </tileset>
