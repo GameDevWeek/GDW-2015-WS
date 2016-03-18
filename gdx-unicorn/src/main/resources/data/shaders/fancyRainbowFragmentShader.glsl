4459b0251137b28bf4e95d3c2a07caafef2bf5d7
Armin Willer <a.willer@hochschule-trier.de>
2016-03-18 18:23:44 +0100

    debuggin

--- gdx-unicorn/src/main/resources/data/shaders/fancyRainbowFragmentShader.glsl
+++ gdx-unicorn/src/main/resources/data/shaders/fancyRainbowFragmentShader.glsl
@@ -14,6 +14,7 @@ uniform float u_rainbowFrequency;
 uniform float u_rainbowAmplitude;
 uniform float u_time;
 uniform sampler2D u_texture;
+uniform mat4 u_projTrans;
 
 vec4 calcRainbowColor();
 float getHorizontalOffset();
@@ -21,7 +22,7 @@ float getHorizontalOffset();
 void main()
 {
 	// use uniforms so they do not get removed
-	vec4 tex = texture2D(u_texture, a_texCoord0);
+	vec4 tex = u_projTrans * texture2D(u_texture, a_texCoord0);
 	//vec4 rainbowColor = v_color * (1.0 - v_rainbowAlpha) + calcRainbowColor() * v_rainbowAlpha;
 	
 	gl_FragColor = calcRainbowColor();
