05279b6d7f7e96d07f1d69f9c107847c6d1e5e92
WeraGossner <W.Gossner@gmx.net>
2016-03-18 10:54:51 +0100

    Refferrenzpfade in den Tilesets und Level 1 angepasst

--- gdx-unicorn/src/main/resources/data/maps/tilesets/characters_tileset_final.tsx
+++ gdx-unicorn/src/main/resources/data/maps/tilesets/characters_tileset_final.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="characters_tileset" tilewidth="64" tileheight="64" tilecount="9" columns="3">
- <image source="../Graphics/entities/characters_tileset.png" width="192" height="192"/>
+ <image source="characters_tileset.png" width="192" height="192"/>
 </tileset>
