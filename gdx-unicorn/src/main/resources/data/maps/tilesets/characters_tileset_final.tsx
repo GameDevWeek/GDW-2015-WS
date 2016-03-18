aa3f3c792e8b89a2e784e687502d0050c533276c
unknown <rftpool13@rft1305.rz.fh-trier.de>
2016-03-18 09:01:56 +0100

    IF-Abfrage auskommentiert im Entity Loader Source in Tilesets korrekt gesetzt PNG in Tilesets hinzugefügt lvl1.tmx überarbeitet

--- gdx-unicorn/src/main/resources/data/maps/tilesets/characters_tileset_final.tsx
+++ gdx-unicorn/src/main/resources/data/maps/tilesets/characters_tileset_final.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="characters_tileset" tilewidth="64" tileheight="64" tilecount="9" columns="3">
- <image source="../Graphics/entities/characters_tileset.png" width="192" height="192"/>
+ <image source="../tilesets/characters_tileset.png" width="192" height="192"/>
  <tile id="0">
   <properties>
    <property name="entity_type" value="hunter"/>
--- gdx-unicorn/src/main/resources/data/maps/tilesets/entities_tileset_final.tsx
+++ gdx-unicorn/src/main/resources/data/maps/tilesets/entities_tileset_final.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="entities_tileset_final" tilewidth="64" tileheight="64" tilecount="100" columns="10">
- <image source="../Graphics/entities/entities_tileset_final.png" width="640" height="640"/>
+ <image source="../tilesets/entities_tileset.png" width="640" height="640"/>
  <tile id="23">
   <properties>
    <property name="entity_type" value="chocolate"/>
@@ -8,12 +8,12 @@
  </tile>
  <tile id="25">
   <properties>
-   <property name="entity_type" value="rainbow_bubblegum"/>
+   <property name="entity_type" value="bubblegum_rainbow"/>
   </properties>
  </tile>
  <tile id="26">
   <properties>
-   <property name="entity_type" value="blue_bubblegum"/>
+   <property name="entity_type" value="bubblegum_blue"/>
   </properties>
  </tile>
 </tileset>
