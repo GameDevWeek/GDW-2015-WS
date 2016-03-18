cd3798c2af9ea470842ab8c81c3ddebb1ee9a91e
WeraGossner <W.Gossner@gmx.net>
2016-03-18 14:08:51 +0100

    Weitere Tilesets und Level 1

---
+++ gdx-unicorn/src/main/resources/data/maps/tilesets/platform4x1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="platform4x1" tilewidth="256" tileheight="32" tilecount="1" columns="1">
+ <image source="tilesets/platform4x1.png" width="256" height="32"/>
+</tileset>
