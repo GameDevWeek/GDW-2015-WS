b00243ea3ac2b9830382a133b8ea7000c69a0117
Lusito <core@roughael.net>
2016-03-19 17:46:20 +0100

    pickups blend in

--- gdx-ugotpaint/src/main/java/de/hochschuletrier/gdw/ws1516/game/systems/PickupSystem.java
+++ gdx-ugotpaint/src/main/java/de/hochschuletrier/gdw/ws1516/game/systems/PickupSystem.java
@@ -3,34 +3,37 @@ package de.hochschuletrier.gdw.ws1516.game.systems;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.math.Rectangle;
 import de.hochschuletrier.gdw.ws1516.events.PickupEvent;
+import de.hochschuletrier.gdw.ws1516.game.ComponentMappers;
 import de.hochschuletrier.gdw.ws1516.game.Game;
 import de.hochschuletrier.gdw.ws1516.game.GameConstants;
+import de.hochschuletrier.gdw.ws1516.game.components.AnimationComponent;
+import de.hochschuletrier.gdw.ws1516.game.components.PickupComponent;
 import de.hochschuletrier.gdw.ws1516.game.utils.PlayerColor;
 import java.util.Random;
 
-public class PickupSystem extends EntitySystem implements PickupEvent.Listener {
+public class PickupSystem extends IteratingSystem implements PickupEvent.Listener {
 
     private final Random rand = new Random();
     private final Game game;
 
-    public PickupSystem(Game game) {
-        this(game, 0);
-    }
-
     public PickupSystem(Game game, int priority) {
-        super(priority);
+        super(Family.all(PickupComponent.class).get(), priority);
         this.game = game;
     }
 
     @Override
     public void addedToEngine(Engine engine) {
+        super.addedToEngine(engine);
         PickupEvent.register(this);
     }
 
     @Override
     public void removedFromEngine(Engine engine) {
+        super.removedFromEngine(engine);
         PickupEvent.unregister(this);
     }
 
@@ -43,6 +46,17 @@ public class PickupSystem extends EntitySystem implements PickupEvent.Listener {
         Rectangle b = GameConstants.PICKUP_SPAWN_RECT;
         float x = b.x + (rand.nextFloat() * b.width - 1);
         float y = b.y + (rand.nextFloat() * b.height - 1);
-        game.createEntity("pickup", x, y, PlayerColor.NEUTRAL);
+        Entity entity = game.createEntity("pickup", x, y, PlayerColor.NEUTRAL);
+        ComponentMappers.animation.get(entity).alpha = 0;
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        AnimationComponent anim = ComponentMappers.animation.get(entity);
+        if(anim.alpha < 1) {
+            anim.alpha += deltaTime;
+            if(anim.alpha > 1)
+                anim.alpha = 1;
+        }
     }
 }
