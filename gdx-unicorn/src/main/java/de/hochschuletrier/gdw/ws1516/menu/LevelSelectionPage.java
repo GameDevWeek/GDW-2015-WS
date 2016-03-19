1fdb898a652839b6488908da4b52be5a41687685
unknown <rftpool13@rft1312.rz.fh-trier.de>
2016-03-19 13:41:52 +0100

    changed some stuff (tried to fix)

--- gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/menu/LevelSelectionPage.java
+++ gdx-unicorn/src/main/java/de/hochschuletrier/gdw/ws1516/menu/LevelSelectionPage.java
@@ -12,10 +12,6 @@ import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.gdx.menu.MenuManager;
-<<<<<<< HEAD
-=======
-import de.hochschuletrier.gdw.commons.gdx.menu.widgets.DecoImage;
->>>>>>> 6d41a4c33afac4879876be91aabc33c3b610120e
 import de.hochschuletrier.gdw.commons.gdx.state.transition.SplitHorizontalTransition;
 import de.hochschuletrier.gdw.commons.gdx.utils.DrawUtil;
 import de.hochschuletrier.gdw.commons.jackson.JacksonReader;
@@ -42,11 +38,7 @@ public class LevelSelectionPage extends MenuPage {
     public LevelSelectionPage(Skin skin, MenuManager menuManager) {
         super(skin, "menu_bg");
         int xOffset=20;
-<<<<<<< HEAD
         int xStep=125;
-=======
-        int xStep=150;
->>>>>>> 6d41a4c33afac4879876be91aabc33c3b610120e
         int i = 0;
         Main.getInstance().screenCamera.bind();
         
@@ -67,36 +59,7 @@ public class LevelSelectionPage extends MenuPage {
         Texture level_preview_texture = assetManager.getTexture(levelNames.get(0));
         Texture buttonBack_texture = assetManager.getTexture("prev_Button");
         Texture buttonNext_texture = assetManager.getTexture("next_Button");
-<<<<<<< HEAD
         
-=======
-
-
-      
-    
-        
-        level_previews = new Texture[level_preview_count];
-      
-        level_previews[0] = assetManager.getTexture("level1");
-        level_previews[1] = assetManager.getTexture("level2");
-        level_previews[2] = assetManager.getTexture("level3");
-        level_previews[3] = assetManager.getTexture("level4");
-        
-        level_preview = createImageButton(level_previews[level_preview_index], 312, 260, 50, 50, this::nextLevel, "einhornMotivated", true, false);
-
-        level_previews = new Texture[levelNames.size()];
-        for (String levelName : levelNames) {
-            level_previews[level_preview_count] = assetManager.getTexture(levelName);
-            level_preview_count++;
-        }
-      
-        level_preview = createImageButton(level_previews[level_preview_index], 312, 280, 50, 50, this::nextLevel, "einhornMotivated", true, false);
-
-                    
-        createImageButton(buttonBack_texture, 310-20-buttonBack_texture.getWidth(), 260, 50, 50, this::previousLevel, "buttonSound", true, true);
-        createImageButton(buttonNext_texture, 310+level_preview_texture.getWidth()+20, 260, 50, 50, this::nextLevel, "buttonSound", true, true);
-
->>>>>>> 6d41a4c33afac4879876be91aabc33c3b610120e
         level_preview_d = new DecoImage(assetManager.getTexture(levelNames.get(level_preview_index)));
         
         level_previews = new Texture[levelNames.size()];
@@ -110,7 +73,6 @@ public class LevelSelectionPage extends MenuPage {
                       
         createImageButton(buttonBack_texture, 310-20-buttonBack_texture.getWidth(), 260, 50, 50, this::previousLevel, "buttonSound", true, true);
         createImageButton(buttonNext_texture, 310+level_preview_texture.getWidth()+20, 260, 50, 50, this::nextLevel, "buttonSound", true, true);
-<<<<<<< HEAD
         
         addCenteredButton(512, 200, 50, 50, "Spielen", this::startGame, "einhornMotivated");
         addLeftAlignedButton(55, 40, 100, 50, "Zurück", () -> menuManager.popPage(),"zurueck");
@@ -118,31 +80,11 @@ public class LevelSelectionPage extends MenuPage {
 //        addLabeledTexture("coin_hud", "1 Punkte", xOffset+xStep*(i++)-15,550,0,40,40);
 //        addLabeledTexture("drop", "3 Punkte", xOffset+xStep*(i++), 550,0,40,40);
 //        addLabeledTexture("gum_hud", "Kaugummi", xOffset+xStep*(i++), 550,0,40,40);
-=======
-
-        
-        addCenteredButton(512, 200, 50, 50, "Spielen", this::startGame, "einhornMotivated");
-        addLeftAlignedButton(55, 40, 100, 50, "Zurück", () -> menuManager.popPage(),"zurueck");
-
-        
-        addLabeledTexture("heart3", "Leben",xOffset+xStep*(i++),550,0,0,40,40);
-        addLabeledTexture("coin_hud", "1 Punkte", xOffset+xStep*(i++)-15,550,0,0,40,40);
-        addLabeledTexture("drop", "3 Punkte", xOffset+xStep*(i++), 550,0,0,40,40);
-        addLabeledTexture("gum_hud", "Kaugummi", xOffset+xStep*(i++), 550,0,0,40,40);
-        addLabeledTexture("hunter", "Jäger",xOffset-5,450,20,0,64,64);
-        addLabeledTexture("paparazzi", "Paparazzi",xOffset-10,300,5,0,64,64);
-
-
->>>>>>> 6d41a4c33afac4879876be91aabc33c3b610120e
         
         super.addActor(level_preview_d);
         
         
         
-<<<<<<< HEAD
-=======
-        
->>>>>>> 6d41a4c33afac4879876be91aabc33c3b610120e
     }
     
     private void setLevel(int index) {
