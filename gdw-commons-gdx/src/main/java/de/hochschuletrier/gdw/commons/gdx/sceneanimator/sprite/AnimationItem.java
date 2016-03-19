69ff296ae6e576f427e9f2d8153a21290eb4859d
Lusito <core@roughael.net>
2016-03-19 14:52:06 +0100

    flip x/y support

--- gdw-commons-gdx/src/main/java/de/hochschuletrier/gdw/commons/gdx/sceneanimator/sprite/AnimationItem.java
+++ gdw-commons-gdx/src/main/java/de/hochschuletrier/gdw/commons/gdx/sceneanimator/sprite/AnimationItem.java
@@ -1,5 +1,6 @@
 package de.hochschuletrier.gdw.commons.gdx.sceneanimator.sprite;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import de.hochschuletrier.gdw.commons.gdx.assets.AnimationExtended;
@@ -18,12 +19,16 @@ public class AnimationItem extends Item {
     protected float scale;
     protected AnimationExtended animation;
     private final Getter getter;
+    private final boolean flipX;
+    private final boolean flipY;
 
-    public AnimationItem(String group, float scale, float startTime, float angle, boolean oriented, float opacity, String animation, Getter getter) {
+    public AnimationItem(String group, float scale, float startTime, float angle, boolean oriented, boolean flipX, boolean flipY, float opacity, String animation, Getter getter) {
         super(group, startTime, 0, oriented, opacity);
         this.angleAdd = angle;
         this.scale = scale;
         this.startTime = startTime;
+        this.flipX = flipX;
+        this.flipY = flipY;
         this.getter = getter;
         this.animationTime = 0;
         this.animation = getter.getAnimation(animation);
@@ -44,7 +49,11 @@ public class AnimationItem extends Item {
             float h = r.getRegionHeight();
             float hw = w* 0.5f;
             float hh = h* 0.5f;
-            DrawUtil.batch.draw(r, position.x - hw, position.y - hh, hw, hh, w, h, scale, scale, angle + angleAdd);
+            float x = position.x - hw;
+            float y = position.y - hh;
+            float scaleX = flipX ? -scale : scale;
+            float scaleY = flipY ? scale : -scale;
+            DrawUtil.batch.draw(r, x, y, hw, hh, w, h, scaleX, scaleY, angle + angleAdd);
         }
     }
 
